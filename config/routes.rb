019cb4be02823c8e24efcb2d533fa49d72c29297
Jose Galisteo <ceritium@gmail.com>
2016-11-16 19:09:28 +0100

    pages and posts for demo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root 'pages#index'
+  resources :posts, only: [:index, :show]
+  resources :pages, only: [:index, :show]
 end
